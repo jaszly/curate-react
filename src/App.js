651f95daa721422905c6d3d99c52a01b4f7e0c43
jaszly <jasvmurray@gmail.com>
2019-11-28 15:29:02 +0700

    create react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<h1>what</h1>
+			</div>
+		)
+	}
+}
+export default App
