110705221c2d21a056148efdb010fa0f06ddc28f
jaszly <jasvmurray@gmail.com>
2019-12-01 15:19:05 +0700

    import react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 // import Signup from './components/auth/signup'
 import Spaces from './components/layout/spaces'
 import Space from './components/layout/space'
