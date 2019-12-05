0b5055ca6b47fffddccadbb5b6196d6dbdfca3b8
jaszly <jasvmurray@gmail.com>
2019-12-05 11:47:56 +0700

    add nav to header

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import TopNav from './nav'
+import Filters from './filters'
 
 class Header extends React.Component {
 	render() {
 		return (
 			<div>
+				<TopNav />
 				<div className="hero">
 					<div className="hero-inner">
 						<h1>Curate</h1>
