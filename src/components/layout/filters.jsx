77c37ea35192fdb3d62ceb5a07cfdc4e8666cbd5
jaszly <jasvmurray@gmail.com>
2019-12-09 14:31:51 +0700

    search bar component

--- src/components/layout/filters.jsx
+++ src/components/layout/filters.jsx
@@ -5,17 +5,32 @@ import axios from 'axios'
 export default class Search extends React.Component {
 	state = {
 		initialSpaces: [],
-		spaces: []
+		spaces: [],
+		searchTerm: ''
 	}
 
-	filterSpaces = event => {
-		let spaces = this.state.initialSpaces
-		spaces = spaces.filter(space => {
-			return space.toLowerCase().search(event.target.value.toLowerCase()) !== -1
+	// filterSpaces = event => {
+	// 	let spaces = this.state.initialSpaces
+	// 	spaces = spaces.filter(space => {
+	// 		return space.toLowerCase().search(event.target.value.toLowerCase()) !== -1
+	// 	})
+	// 	this.setState({ spaces: spaces })
+	// }
+
+	searchSpaces = e => {
+		console.log('searchTerm', e.target.value)
+		this.setState({ searchTerm: e.target.value })
+		let spaces = this.state.allSpaces
+		let searchTerm = e.target.value
+		let results = spaces.filter(s => {
+			return (
+				s.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+				s.neighborhood.toLowerCase().includes(searchTerm.toLowerCase()) ||
+				s.city.toLowerCase().includes(searchTerm.toLowerCase())
+			)
 		})
-		this.setState({ spaces: spaces })
+		this.setState({ spaces: results })
 	}
-
 	componentWillMount = () => {
 		this.setState({
 			initialSpaces: this.props.content,
