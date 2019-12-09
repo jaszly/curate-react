59c9261a9dfeec359ac61d20708b621655651f01
jaszly <jasvmurray@gmail.com>
2019-12-09 14:34:05 +0700

    add search bar component

--- src/components/layout/spaces.jsx
+++ src/components/layout/spaces.jsx
@@ -3,11 +3,14 @@ import Cards from './cards.jsx'
 import TopNav from './nav'
 import axios from 'axios'
 import Search from './filters.jsx'
+import Header from './header'
 
 class Spaces extends React.Component {
 	state = {
-		spaces: [],
-		initialSpaces: []
+		spaces: [{ title: '', neighborhood: '', city: '' }],
+		initialSpaces: [],
+		searchTerm: '',
+		newSpaces: []
 	}
 
 	componentWillMount() {
@@ -42,10 +45,44 @@ class Spaces extends React.Component {
 		// 3. reset state of spaces
 	}
 
+	searchSpaces = e => {
+		console.log('searchTerm', e.target.value)
+		// this.setState({ searchTerm: e.target.value })
+		if (e.target.value === '') {
+			console.log('it is empty')
+			let spaces = this.state.initialSpaces
+			this.setState({ spaces })
+			console.log(spaces)
+		} else {
+			console.log('not empty')
+			let filteredSpaces = this.state.initialSpaces.filter(space => {
+				return (
+					space.title.toLowerCase().includes(e.target.value.toLowerCase()) ||
+					space.neighborhood
+						.toLowerCase()
+						.includes(e.target.value.toLowerCase()) ||
+					space.city.toLowerCase().includes(e.target.value.toLowerCase())
+				)
+				console.log('filtered spaces: ', filteredSpaces)
+			})
+			this.setState({ spaces: filteredSpaces })
+		}
+	}
+	// let searchTerm = e.target.value
+	// let results = spaces.filter(s => {
+	// 	return (
+	// 		s.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+	// 		s.neighborhood.toLowerCase().includes(searchTerm.toLowerCase()) ||
+	// 		s.city.toLowerCase().includes(searchTerm.toLowerCase())
+	// 	)
+	// })
+	// this.setState({ spaces: results })
+
 	render() {
 		return (
 			<div>
 				<TopNav filterFunction={this.filterFunction} />
+				<Header searchSpaces={this.searchSpaces} />
 				<div className="grid small five">
 					{this.state.spaces.map((space, i) => (
 						<Cards space={space} key={i} />
