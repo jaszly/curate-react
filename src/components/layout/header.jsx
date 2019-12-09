d269b24277dbfc0751d2ace15abb815dd786e4a9
jaszly <jasvmurray@gmail.com>
2019-12-09 14:33:32 +0700

    add search bar to header

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,20 +1,46 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import TopNav from './nav'
-import Filters from './filters'
+import Search from './filters'
+
+// import Filters from './filters'
 
 class Header extends React.Component {
 	render() {
 		return (
 			<div>
-				<TopNav />
-				<div className="hero">
-					<div className="hero-inner">
-						<h1>Curate</h1>
-						<h2>Stylish Spaces for Content Creators</h2>
+				<div className="hero-img">
+					<div className="ctnr box">
+						<h2 style={{ fontFamily: 'Montserrat', fontSize: '40px' }}>
+							What are you looking for?
+						</h2>
+						<span
+							style={{
+								fontFamily: 'Montserrat',
+								letterSpacing: '.5px',
+								fontSize: '20px'
+							}}
+						>
+							Find the perfect space for your creative needs
+						</span>
+						<div> </div>
+						<div>
+							<input
+								className="ctnr-search"
+								type="text"
+								placeholder="    Search"
+								onChange={e => this.props.searchSpaces(e)}
+								style={{
+									width: '320px',
+									height: '40px',
+									backgroundPosition: 'center',
+									borderRadius: '30px',
+									color: '#333'
+								}}
+							/>
+						</div>
 					</div>
 				</div>
-				<main>content</main>
 			</div>
 		)
 	}
