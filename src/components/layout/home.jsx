26311dd65a5baa3631fd150c3962fd320205840e
jaszly <jasvmurray@gmail.com>
2019-12-09 14:30:14 +0700

    create home page

--- src/components/layout/home.jsx
+++ src/components/layout/home.jsx
@@ -1,30 +1,50 @@
 import React from 'react'
-import Header from './header'
+import { Link } from 'react-router-dom'
 import TopNav from './nav'
-import axios from 'axios'
-import Search from './filters.jsx'
-import { Container, Row, Col } from 'reactstrap'
+
+import Filters from './filters'
 
 class Home extends React.Component {
-	componentWillMount() {
-		axios
-			.get('http://localhost:4000/')
-			.then(res => {
-				console.log('res.data:', res.data)
-			})
-			.catch(err => {
-				console.log(err)
-			})
-	}
-
 	render() {
 		return (
 			<div>
-				<Header />
-				<h2>how it works</h2>
+				<TopNav />
+				<div className="bg-img">
+					<div className="ctnr ">
+						<h2 style={{ fontFamily: 'Montserrat', fontSize: '45px' }}>
+							Curated Spaces <br /> for Content Creators
+						</h2>
+						<span
+							style={{
+								fontFamily: 'Montserrat',
+								letterSpacing: '.5px',
+								fontSize: '20px'
+							}}
+						>
+							Find the perfect space for your creative needs
+						</span>
+						<div> </div>
+						<div>
+							<Link
+								to="/spaces"
+								className="primary"
+								style={{
+									fontFamily: 'Lato',
+									fontSize: '1rem',
+									backgroundColor: 'tomato',
+									width: '200px',
+									opacity: '80%',
+									backgroundPosition: 'center',
+									marginTop: '15px'
+								}}
+							>
+								Browse Spaces
+							</Link>
+						</div>
+					</div>
+				</div>
 			</div>
 		)
 	}
 }
-
 export default Home
